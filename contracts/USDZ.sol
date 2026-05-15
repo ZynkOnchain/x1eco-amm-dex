b8746aef9c736559cada990d65291f7885a89653
Zynk <185857603+ZynkOnchain@users.noreply.github.com>
2026-05-15 20:25:11 +0530

    Update USDZ.sol

--- contracts/USDZ.sol
+++ contracts/USDZ.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDZ is ERC20, Ownable {
+
+    uint256 public constant MAX_SUPPLY =
+        1000000000 * 10**18;
+
+    constructor()
+        ERC20("USD Zynk", "USDZ")
+        Ownable(msg.sender)
+    {
+        _mint(
+            msg.sender,
+            MAX_SUPPLY
+        );
+    }
+
+    function burn(uint256 amount)
+        external
+    {
+        _burn(
+            msg.sender,
+            amount
+        );
+    }
+}
